360f7f9ced5ef5523d7b1a8cdc903da861200ba6
Mo Omer <momer@c-k.com>
2021-02-11 15:26:18 -0600

    Our first integration

--- lib/linked_in/client/profiles.rb
+++ lib/linked_in/client/profiles.rb
@@ -2,10 +2,10 @@ module LinkedInAPI
   class Client
     # https://docs.microsoft.com/en-us/linkedin/shared/integrations/people/profile-api
     module Profiles
-      def me(projection = "")
+      def me(projection = nil)
         options = {}
         # Optionals
-        options.merge!(projection: projection) if projection.present?
+        options.merge!(projection: projection) if projection
 
         get(Configuration::API_PREFIX + "me", options)
       end
@@ -13,10 +13,10 @@ module LinkedInAPI
       # https://api.linkedin.com/v2/people/
       # https://docs.microsoft.com/en-us/linkedin/shared/integrations/people/profile-api
       # GET https://api.linkedin.com/v2/people/(id:{profile ID})?projection=(id,firstName,lastName)
-      def people(profile_id, projection = "")
+      def people(profile_id, projection = nil)
         options = {}
         # Optionals
-        options.merge!(projection: projection) if projection.present?
+        options.merge!(projection: projection) if projection
 
         get(Configuration::API_PREFIX + "people/(id:#{profile_id})", options)
       end
--- lib/linked_in/request.rb
+++ lib/linked_in/request.rb
@@ -6,29 +6,29 @@ module LinkedInAPI
   # Defines HTTP request methods
   module Request
     # Perform an HTTP GET request
-    def get(path, options={}, signature=false, raw=false, no_response_wrapper=no_response_wrapper(), signed=sign_requests)
+    def get(path, options = {}, signature = false, raw = false, no_response_wrapper = no_response_wrapper(), signed = sign_requests)
       request(:get, path, options, signature, raw, no_response_wrapper, signed)
     end
 
     # Perform an HTTP POST request
-    def post(path, options={}, signature=false, raw=false, no_response_wrapper=no_response_wrapper(), signed=sign_requests)
+    def post(path, options = {}, signature = false, raw = false, no_response_wrapper = no_response_wrapper(), signed = sign_requests)
       request(:post, path, options, signature, raw, no_response_wrapper, signed)
     end
 
     # Perform an HTTP PUT request
-    def put(path, options={},  signature=false, raw=false, no_response_wrapper=no_response_wrapper(), signed=sign_requests)
+    def put(path, options = {}, signature = false, raw = false, no_response_wrapper = no_response_wrapper(), signed = sign_requests)
       request(:put, path, options, signature, raw, no_response_wrapper, signed)
     end
 
     # Perform an HTTP DELETE request
-    def delete(path, options={}, signature=false, raw=false, no_response_wrapper=no_response_wrapper(), signed=sign_requests)
+    def delete(path, options = {}, signature = false, raw = false, no_response_wrapper = no_response_wrapper(), signed = sign_requests)
       request(:delete, path, options, signature, raw, no_response_wrapper, signed)
     end
 
     private
 
     # Perform an HTTP request
-    def request(method, path, options, signature=false, raw=false, no_response_wrapper=false, signed=sign_requests)
+    def request(method, path, options, signature = false, raw = false, no_response_wrapper = false, signed = sign_requests)
       response = connection(raw).send(method) do |request|
         case method
         when :get, :delete
@@ -41,8 +41,13 @@ module LinkedInAPI
 
       return response if raw
       return response.body if no_response_wrapper
-      return Response.create( response.body, {:limit => response.headers['x-ratelimit-limit'].to_i,
-                                              :remaining => response.headers['x-ratelimit-remaining'].to_i} )
+
+      ratelimit_hash = {}
+
+      ratelimit_hash.merge!(limit: response.headers['x-ratelimit-limit'].to_i) if response.headers['x-ratelimit-limit']
+      ratelimit_hash.merge!(remaining: response.headers['x-ratelimit-remaining'].to_i) if response.headers['x-ratelimit-remaining']
+
+      return Response.create(response.body, ratelimit_hash)
     end
   end
 end
